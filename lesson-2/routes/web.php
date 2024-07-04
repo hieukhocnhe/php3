35d6a695fd1621f748e01d824a59505cea97f7b7
Tran Chung Hieu <trantrunghieu422@gmail.com>
2024-07-04 23:51:27 +0700

    Lesson 3 done

--- lesson-2/routes/web.php
+++ lesson-2/routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('home', function () {
+    return view('home');
+});
+
+// Route::get('product/{id}', function ($id) {
+//     return "Product id : $id";
+// })->where('id','[0-9]+');
+
+Route::view('about', 'about')->name('page.about');
+
+Route::get('/user/{id}/comment/{comment_id?}', function ($id, $comment_id = null) {
+    return "User id : $id - Comment id : $comment_id";
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('products', function () {
+        return "Products";
+    });
+    Route::get('users', function () {
+        return "Users";
+    });
+});
